9afe34d74421f7008b7aaebe820859ecee662225
Md Rifat <mdrifatahmed787@gmail.com>
2024-06-30 11:35:45 +0600

    cpp

---
+++ class_object.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+  public:
+   char name[100];
+   int roll;
+   double cgpa;
+}
+ 
+int main(){
+   Student rifat;
+   rifat.
+
+    
+    return 0;
+}
